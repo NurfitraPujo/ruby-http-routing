8c88a8144287252647eb12783dd43c6ca83df3aa
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-07-14 22:36:39 +0700

    fixed db module loading

--- web.rb
+++ web.rb
@@ -1,42 +1,45 @@
 require 'sinatra'
+require_relative './lib/db'
+
+db_con = DatabaseConnection.new
 
 get '/message' do
-    "<h1 style=\"font-size:24px;\">Hello world</h1>"
+  '<h1 style="font-size:24px;">Hello world</h1>'
 end
 
 get '/message/:name' do
-    name = params[:name]
-    color = params[:color] ? params[:color] : 'rebeccapurple'
-    erb :message, locals: {
-        color: color,
-        name: name
-    }
+  name = params[:name]
+  color = params[:color] || 'rebeccapurple'
+  erb :message, locals: {
+    color: color,
+    name: name
+  }
 end
 
 get '/login' do
-    erb :login
+  erb :login
 end
 
 post '/login' do
-    if params[:username] == 'admin' && params[:password] == 'admin'
-        return 'Logged in'
-    else
-        erb :login
-    end
+  if params[:username] == 'admin' && params[:password] == 'admin'
+    return 'Logged in'
+  else
+    erb :login
+  end
 end
 
-items = ['ramen', 'sushi']
+items = %w[ramen sushi]
 
 get '/items' do
-    erb :items, locals: {
-        items: items
-    }
+  erb :items, locals: {
+    items: items
+  }
 end
 
 post '/items' do
-    new_item = params[:new_item]
-    items << new_item unless new_item == ""
-    erb :items, locals: {
-        items: items
-    }
+  new_item = params[:new_item]
+  items << new_item unless new_item == ''
+  erb :items, locals: {
+    items: items
+  }
 end
