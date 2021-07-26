a1e410edda54e4858ec5bfb1de72aa944ec11e64
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-07-26 07:52:11 +0700

    decompose model and view method

--- controller/item_controller.rb
+++ controller/item_controller.rb
@@ -4,29 +4,40 @@ class ItemController
   def create_new(item_data = {})
     raise ArgumentError if item_data.nil?
 
-    item = Item.new(item_data)
+    item_categories = []
+    unless item_data[:categories].empty?
+      item_data[:categories].each do |id|
+        category = { id: id }
+        item_categories << category
+      end
+    end
+
+    item = Item.new(id: item_data[:id], nama: item_data[:nama], price: item_data[:price], categories: item_categories)
     item.save
   end
 
   def find_all
-    items = Item.all
+    Item.all
+  end
 
+  def all_items
+    items = find_all
     renderer = ERB.new(File.read('./views/items.erb'))
     renderer.result(binding)
   end
 
   def find_by_id(item_id)
-    item = Item.where(column: 'items.id', value: item_id)[0]
+    Item.where(column: 'items.id', value: item_id)[0]
+  end
 
+  def item_details(item_id)
+    item = find_by_id(item_id)
     renderer = ERB.new(File.read('./views/item.erb'))
     renderer.result(binding)
   end
 
   def update(item_data = {})
-    items = Item.update(item_data)
-
-    renderer = ERB.new(File.read('./views/items.erb'))
-    renderer.result(binding)
+    Item.update(item_data)
   end
 
   def delete(item_id)
