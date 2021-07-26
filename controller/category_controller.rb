352193bbf1fb8b48056e7b69822b3bb84f1005e0
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-07-26 07:51:54 +0700

    decompose model method with view method, added find_by_id, update & delete

--- controller/category_controller.rb
+++ controller/category_controller.rb
@@ -9,14 +9,25 @@ class CategoryController
   end
 
   def find_all
-    categories = Category.all
+    Category.all
+  end
 
+  def find_by_id(category_id)
+    Category.where(category_id)[0]
+  end
+
+  def show_all_categories
+    categories = find_all
     renderer = ERB.new(File.read('./views/categories.erb'))
     renderer.result(binding)
   end
 
   def find_all_category_items(category_id)
-    category = Category.query_category_items(category_id)
+    Category.query_category_items(category_id)
+  end
+
+  def show_category_items(category_id)
+    category = find_all_category_items(category_id)
 
     renderer = ERB.new(File.read('./views/category_items.erb'))
     renderer.result(binding)
@@ -25,5 +36,13 @@ class CategoryController
   def creating_new_category
     renderer = ERB.new(File.read('./views/add_category.erb'))
     renderer.result(binding)
+  end
+
+  def update(category_data = {})
+    Category.update(category_data)
+  end
+
+  def delete(category_id)
+    Category.delete(category_id)
   end
 end
